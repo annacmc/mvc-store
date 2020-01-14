e19a7a8af71cd3eeec9084d877c301a93cbaa961
Anna McPhee <30754158+annacmc@users.noreply.github.com>
2020-01-14 14:51:00 +0900

    Updated viewproducts.blade
    Updated viewproducts design to incorporate product vars

--- resources/views/viewproducts.blade.php
+++ resources/views/viewproducts.blade.php
@@ -8,9 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
     <!-- Custom stylesheets-->
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css" >
-    <!-- Add Logo as Favicon -->
-    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
-    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+
   </head>
   <body>
       <!-- begin blog header content -->
@@ -23,10 +21,9 @@
         <a class="blog-header-logo text-dark" href="#">Anna's Travel Guides</a>
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
-        <a class="text-muted" href="#" aria-label="Search">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24" focusable="false"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
-        </a>
-        <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+<a class="btn btn-sm btn-outline-secondary" href="{{ config('app.url')}}/products/create">Create Product</a>
+
+
       </div>
     </div>
   </header>
@@ -40,6 +37,7 @@
       @foreach ($allProducts as $product)
       <!--output each category as a menu item-->
         <a class="p-2 text-muted" href="index.php?i=cat">{{ $product->category }}</a>
+
       @endforeach
   </nav>
 </div>
@@ -47,17 +45,17 @@
 
       <div class="flex-center position-ref full-height">
           <div class="content">
-              <h1>Here's a list of available products</h1>
               <!--begin container for products-->
               <div class="container">
                   <div class="row">
                       @foreach ($allProducts as $product)
+
                       <!--output each product as a bootstrap card-->
                               <div class="col-md-4">
                                 <div class="card mb-4 shadow-sm">
-                                  <img src="#" class="card-img-top" style="object-fit:cover" height="250px" alt="{{ $product->name }}">
+                                  <img src="{{ $product->cover_image }}" class="card-img-top" style="object-fit:cover" height="250px" alt="{{ $product->name }}">
                                   <div class="card-body">
-                                    <h5 class="card-title">{{ $product->name }}</h5>
+                                    <h5 class="card-title">{{ $product->name }} | {{ $product->category }}</h5>
                                     <p class="card-text">{{ $product->description }}</p>
                                     <div class="d-flex justify-content-between align-items-center">
                                       <div class="btn-group">
@@ -75,7 +73,12 @@
         </div>
     </div>
 
-
+    <footer class="blog-footer">
+      <p>Site by Anna McPhee.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
 
   </body>
   </html>

e4a3c282bf4357ef4529ac1c0023212111997465
Anna McPhee <30754158+annacmc@users.noreply.github.com>
2020-01-31 16:19:16 +0700

    Add shopping cart routes

--- routes/web.php
+++ routes/web.php
@@ -24,17 +24,8 @@ Route::get('/createproduct', function () {
     return view('createproduct');
 });
 
-Route::get('/shopping-cart-add', function () {
-    Cart::add(1, 'Macbook Pro', 2900, 1, array());
 
-    Cart::update(1,[
-        'quantity' => $quantity
-    ]);
-
-    foreach (Cart::getContent() as $product){
-        echo "Id: $product->id</br>";
-        echo "Name: $product->name</br>";
-        echo "Price $product->price</br>";
-        echo "Quantity $product->quantity</br>";
-    }
-});
+Route::get('/', 'HomeController@index')->name('home');
+Route::post('/cart-add', 'CartController@add')->name('cart.add');
+Route::get('/cart-checkout', 'CartController@cart')->name('cart.checkout');
+Route::post('/cart-clear', 'CartController@clear')->name('cart.clear');

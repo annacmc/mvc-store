b70e48461996df5e78106e7a4e786ce16e10d184
Anna McPhee <30754158+annacmc@users.noreply.github.com>
2020-01-31 22:47:11 +0700

    Edited home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,30 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Brand;
 use App\Product;
+use App\Category;
+use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function index()
     {
-        $this->middleware('auth');
+        $params = [
+            'title' => 'Annas Travel Guides',
+            'sub_title' => 'All Guides Listing',
+            'products' => Product::all(),
+        ];
+
+        return view('index')->with($params);
     }
 
-    public function index()
-        {
-            $params = [
-                'title' => 'Annas Travel Guides',
-                'sub_title' => 'All Products Listing',
-                'products' => Product::all(),
-            ];
+    public function brands($url)
+    {
+        $brand = Brand::whereUrl($url)->first();
 
-            return view('home')->with($params);
-        }
+        $params = [
+            'title' => $brand->title,
+            'sub_title' => "Products by $brand->title",
+            'products' => $brand->products,
+        ];
 
+        return view('index')->with($params);
+    }
+
+    public function categories($url)
+    {
+        $category = Category::whereUrl($url)->first();
+
+        $params = [
+            'title' => $category->title,
+            'sub_title' => "Products in $category->title category",
+            'products' => $category->products,
+        ];
+
+        return view('index')->with($params);
+    }
 }

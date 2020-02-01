7a653635c9ba5e718870176d60552e610a4a03ad
Anna McPhee <30754158+annacmc@users.noreply.github.com>
2020-02-01 07:38:34 +0700

    Styling product creation form

--- resources/views/createproduct.blade.php
+++ resources/views/createproduct.blade.php
@@ -1,12 +1,16 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('content')
 
-@section("content")
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Create a new Product</div>
+
+                <div class="card-body">
 
                 <form method="POST" action="{{ config('app.url')}}/products" enctype="multipart/form-data">
                     <!-- CSRF Token -->
                     @csrf
-
-                    <h1> Enter Details to create a product</h1>
                     <div class="form-group">
                         <label>Name</label> <input type="text" name="name">
                     </div>
@@ -20,10 +24,6 @@
                     </div>
 
                     <div class="form-group">
-                        <label>Count</label> <input type="number" name="count">
-                    </div>
-
-                    <div class="form-group">
                         <label>Price</label> <input type="number" step="any" name="price">
                     </div>
                     <div class="form-group">
@@ -34,4 +34,10 @@
                     <button type="submit">Submit</button>
                 </form>
 
+            </form>
+        </div>
+    </div>
+</div>
+</div>
+</div>
 @endsection
